bd7773cbc464d01d52b7841fe47ac96f6f34eadb
Heitor Silva <heitor-silva@hotmail.com>
2018-02-05 20:41:13 -0200

    Add CSS to the component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Countdown Champ, App Component</div>
+      <div class="App">
+        <h1 class="App-title">Countdown to December 25, 2018</h1>
+        <div class="Clock">
+          <div class="Clock-element Clock-days">14 days</div>
+          <div class="Clock-element Clock-hours">30 hours</div>
+          <div class="Clock-element Clock-minutes">15 minutes</div>
+          <div class="Clock-element Clock-seconds">20 seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date" />
+          <button>Submit</button>
+        </div>
+      </div>
     )
   }
 }
