91e90567f2bbd92abc08635feb5a93d6b24eebc3
Heitor Silva <heitor-silva@hotmail.com>
2018-02-05 20:59:28 -0200

    Add changeDeadline method to the component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ class App extends Component {
     };
   }
 
+  changeDeadline() {
+    this.setState({deadline: 'November 25, 2018'})
+  }
+
   render() {
     return (
       <div class="App">
@@ -21,7 +25,7 @@ class App extends Component {
         </div>
         <div>
           <input placeholder="new date" />
-          <button>Submit</button>
+          <button onClick={() => this.changeDeadline()}>Submit</button>
         </div>
       </div>
     )
